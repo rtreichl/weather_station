c7d726c926b22700a731435dd03834d5c968c655
Richard Treichl <Richard Treichl>
2016-02-26 14:48:53 +0100

    ADD: Added files for driver lib. Driver lib will be moved out of this repo. to a independently one

---
+++ msp430/OutdoorSensor/driver/intern/uart/uart.h
@@ -0,0 +1,24 @@
+///----------------------------------------------------------------------------------------
+///
+/// \file uart.h
+///
+/// \brief 
+///
+/// \date 17.02.2016
+///
+/// \author Richard Treichl
+///
+/// \remark
+///
+/// \todo
+///
+/// \version	1.0
+///
+///----------------------------------------------------------------------------------------
+
+#ifndef DRIVER_INTERN_UART_UART_H_
+#define DRIVER_INTERN_UART_UART_H_
+
+
+
+#endif /* DRIVER_INTERN_UART_UART_H_ */
